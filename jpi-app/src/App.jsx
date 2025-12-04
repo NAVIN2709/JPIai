05a17cbd9c21df5cddf5aa899402b1c8b4160965
navin2709 <navinnit2006@gmail.com>
2025-12-05 01:04:50 +0530

    fixed browser router

--- jpi-app/src/App.jsx
+++ jpi-app/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Home from './pages/Home';
 import LandingPage from './pages/Landing';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/think" element={<Home />} />
       </Routes>
-    </Router>
   )
 }
 export default App;
