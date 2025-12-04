3b1545be987acf96561d913250dc4f91e6198cde
navin2709 <navinnit2006@gmail.com>
2025-12-05 01:06:49 +0530

    updated fixed browerser router

--- jpi-app/src/App.jsx
+++ jpi-app/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Home from './pages/Home';
 import LandingPage from './pages/Landing';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/think" element={<Home />} />
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/think" element={<Home />} />
+    </Routes>
+  );
+};
+
 export default App;
